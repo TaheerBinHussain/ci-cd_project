2e1d651f7aaf291b980524840a5d2a275d006d6b
David <david123@gmail.com>
2025-07-20 15:21:10 +0500

    v2

--- test/unittests.py
+++ test/unittests.py
@@ -1,4 +1,13 @@
-from project import add,mul,div,sub
+from project import add, mul, sub, div
 
-assert add(2,3)==5
-assert mul(2,3)==6
+def test_add():
+    assert add(2, 3) == 5
+
+def test_mul():
+    assert mul(2, 3) == 6
+
+def test_sub():
+    assert sub(5, 2) == 3
+
+def test_div():
+    assert div(10, 2) == 5
